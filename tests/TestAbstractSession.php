c064d75741fba8017529fdfaffbaa1968b2ebe84
Aesonus <corylcomposinger@gmail.com>
2018-02-06 23:39:33 -0600

    Added tests and changed a few ignore settings for testing

---
+++ tests/TestAbstractSession.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace Aesonus\Tests;
+
+/**
+ * Description of newPHPClass
+ *
+ * @author Aesonus <corylcomposinger at gmail.com>
+ */
+class TestAbstractSession extends \PHPUnit\Framework\TestCase
+{
+   
+}
